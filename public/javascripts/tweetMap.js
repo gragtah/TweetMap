0c5bd91fc3b611a8bd8fd517d5e069edb72322d1
Gaurav Ragtah <spideyunlimited@gmail.com>
2015-10-25 23:50:26 -0400

    everything works as expected

--- public/javascripts/tweetMap.js
+++ public/javascripts/tweetMap.js
@@ -1,11 +1,13 @@
-function initialize() {
-  //Setup Google Map
-  var myLatlng = new google.maps.LatLng(17.7850,-12.4183);
+function displayHeatmap(keyword) {
+  var socket = io.connect('http://localhost:8081/');
+
+  var myLatlng = new google.maps.LatLng(28,0);
   var light_grey_style = [{"featureType":"landscape","stylers":[{"saturation":-100},{"lightness":65},{"visibility":"on"}]},{"featureType":"poi","stylers":[{"saturation":-100},{"lightness":51},{"visibility":"simplified"}]},{"featureType":"road.highway","stylers":[{"saturation":-100},{"visibility":"simplified"}]},{"featureType":"road.arterial","stylers":[{"saturation":-100},{"lightness":30},{"visibility":"on"}]},{"featureType":"road.local","stylers":[{"saturation":-100},{"lightness":40},{"visibility":"on"}]},{"featureType":"transit","stylers":[{"saturation":-100},{"visibility":"simplified"}]},{"featureType":"administrative.province","stylers":[{"visibility":"off"}]},{"featureType":"water","elementType":"labels","stylers":[{"visibility":"on"},{"lightness":-25},{"saturation":-100}]},{"featureType":"water","elementType":"geometry","stylers":[{"hue":"#ffff00"},{"lightness":-25},{"saturation":-97}]}];
+
   var myOptions = {
     zoom: 2,
     center: myLatlng,
-    mapTypeId: google.maps.MapTypeId.ROADMAP,
+    mapTypeId: google.maps.MapTypeId.HYBRID,
     mapTypeControl: true,
     mapTypeControlOptions: {
       style: google.maps.MapTypeControlStyle.HORIZONTAL_BAR,
@@ -13,30 +15,32 @@ function initialize() {
     },
     styles: light_grey_style
   };
-  var map = new google.maps.Map(document.getElementById("map_canvas"), myOptions);
   
-  //Setup heat map and link to Twitter array we will append data to
+  var map = new google.maps.Map(document.getElementById("map_canvas"), myOptions);
+
+  // setup heat map and link to twitter array which we will add data to
   var heatmap;
   var liveTweets = new google.maps.MVCArray();
   heatmap = new google.maps.visualization.HeatmapLayer({
     data: liveTweets,
     radius: 25
   });
+
   heatmap.setMap(map);
 
-  if(io !== undefined) {
-    // Storage for WebSocket connections
-    var socket = io.connect('http://localhost:8081/');
+    socket.on("connected", function(r) {
+      // we are ready to start receiving tweets
+      socket.emit("start tweets", keyword);
+    });
 
-    // This listens on the "twitter-steam" channel and data is 
-    // received everytime a new tweet is receieved.
+    // handle each tweet received from twitter stream
     socket.on('twitter-stream', function (data) {
 
-      //Add tweet to the heat map array.
+      // add tweet to the heat map array
       var tweetLocation = new google.maps.LatLng(data.lng,data.lat);
       liveTweets.push(tweetLocation);
 
-      //Flash a dot onto the map quickly
+      // flash tweet location dot on the map
       var image = "stylesheets/small-dot-icon.png";
       var marker = new google.maps.Marker({
         position: tweetLocation,
@@ -46,16 +50,15 @@ function initialize() {
       setTimeout(function(){
         marker.setMap(null);
       },600);
-
     });
 
-    // Listens for a success response from the server to 
-    // say the connection was successful.
-    socket.on("connected", function(r) {
-
-      //Now that we are connected to the server let's tell 
-      //the server we are ready to start receiving tweets.
-      socket.emit("start tweets");
+    socket.on('twitter-mongo-load', function (data) {
+      // add tweet to the heat map array
+      var tweetLocation = new google.maps.LatLng(data.lng,data.lat);
+      liveTweets.push(tweetLocation);
     });
-  }
+}
+
+function setKeywordFilter(keyword) {
+  socket.emit("filter tweets", keyword);
 }
--- server.js
+++ server.js
@@ -7,7 +7,7 @@ var twitter = require('twitter'),
     io = require('socket.io').listen(server);
 
 //Setup twitter stream api
-var twit = new twitter({
+var tw = new twitter({
   consumer_key: 'dWSj5FLKxvAmU4fVlEkdfipdR',
   consumer_secret: 'WRffxPBfZspiBfH43bWRSo5B4ZAueCS6jl04kXddAGabQL0L0r',
   access_token_key: '22242886-f2TuagbNuM3Jhcj8itvMkso6PMQubVDY33CyQrvvB',
@@ -15,73 +15,110 @@ var twit = new twitter({
 }),
 stream = null;
 
-console.log("starting up server");
+var mongodb = require('mongodb');
+var MongoClient = mongodb.MongoClient;
+var mongoURL = 'mongodb://localhost:27017/twitter';
+
+// var AlchemyAPI = require('./alchemyapi');
+// var alchemyapi = new AlchemyAPI();
+
+var keyword;
+
+console.log("started server...");
 //Use the default port (for beanstalk) or default to 8081 locally
 server.listen(process.env.PORT || 8081);
 
 //Setup routing for app
 app.use(express.static(__dirname + '/public'));
 
-//Create web sockets connection.
+//Create web sockets connection
 io.sockets.on('connection', function (socket) {
 
-  socket.on("start tweets", function() {
-
+  socket.on("start tweets", function () {
+    console.log("Starting to send tweets");
     if(stream === null) {
-      //Connect to twitter stream passing in filter for entire world.
-      twit.stream('statuses/filter', {'locations':'-180,-90,180,90'}, function(stream) {
-          stream.on('data', function(data) {
-              // Does the JSON result have coordinates
-              if (data.coordinates){
-                if (data.coordinates !== null){
-                  //If so then build up some nice json and send out to web sockets
-                  var outputPoint = {"lat": data.coordinates.coordinates[0],"lng": data.coordinates.coordinates[1]};
 
-                  socket.broadcast.emit("twitter-stream", outputPoint);
+      //Connect to twitter stream, filter for all possible world locations
+      tw.stream('statuses/filter', {'locations':'-180,-90,180,90'}, function(stream) {
+        // Connect to Mongo to load previous tweets and save new ones received
+        MongoClient.connect(mongoURL, function (err, db) {
+            if (err) throw err
+            console.log('Mongo connection established to ', mongoURL);
+            var collection = db.collection('tweets');
 
-                  //Send out to web sockets channel.
-                  socket.emit('twitter-stream', outputPoint);
-                }
-                else if(data.place){
-                  if(data.place.bounding_box === 'Polygon'){
-                    // Calculate the center of the bounding box for the tweet
-                    var coord, _i, _len;
-                    var centerLat = 0;
-                    var centerLng = 0;
+            var cursor = collection.find();
+            cursor.each(function (err, mongoTweet) {
+              if (err) {
+                console.log(err);
+              } else if (mongoTweet != null){
+                  var outputPoint = {"lat": mongoTweet.latitude,"lng": mongoTweet.longitude};
+                  socket.broadcast.emit("twitter-mongo-load", outputPoint);
+                  socket.emit('twitter-mongo-load', outputPoint);
+              }
+            });
 
-                    for (_i = 0, _len = coords.length; _i < _len; _i++) {
-                      coord = coords[_i];
-                      centerLat += coord[0];
-                      centerLng += coord[1];
+            stream.on('data', function(data) {
+                // Only use tweets with location co-ordinates
+                if (data.coordinates){
+                  if (data.coordinates !== null) {
+
+                    if (keyword === undefined || keyword === null || RegExp("(^|\\s+)#*" + keyword + "\(\\s+|$)", "i").test(data.text)) {
+
+                      var outputPoint = {"lat": data.coordinates.coordinates[0],"lng": data.coordinates.coordinates[1]};
+
+                      socket.broadcast.emit("twitter-stream", outputPoint);
+                      socket.emit('twitter-stream', outputPoint);
                     }
-                    centerLat = centerLat / coords.length;
-                    centerLng = centerLng / coords.length;
 
-                    // Build json object and broadcast it
-                    var outputPoint = {"lat": centerLat,"lng": centerLng};
-                    socket.broadcast.emit("twitter-stream", outputPoint);
-
+                    var tweetToSave = {
+                      tweetId:data.id_Str,
+                      text:data.text,
+                      latitude:data.coordinates.coordinates[0], 
+                      longitude:data.coordinates.coordinates[1]
+                    };
+               /*              
+                    if (data.lang === "en"){
+                      console.log('Language: ' + data.lang);
+                      console.log('Text: ' + data.text);
+                      alchemyapi.sentiment("text", data.text, {}, function(response,err){
+                        if (err) { console.log(err);
+                        } else
+                          console.log("Sentiment: " + response["docSentiment"]["type"]);
+                      });
+                    }
+                */
+                    collection.insert(tweetToSave, function(err, records){
+                      if(err) console.log("Error writing to mongo:" + err);                      
+                    });
                   }
                 }
-              }
-              stream.on('limit', function(limitMessage) {
-                return console.log(limitMessage);
-              });
+            });
 
-              stream.on('warning', function(warning) {
-                return console.log(warning);
-              });
-
-              stream.on('disconnect', function(disconnectMessage) {
-                return console.log(disconnectMessage);
-              });
-          });
+            stream.on('limit', function(limitMessage) {
+              return console.log(limitMessage);
+            });
+            stream.on('warning', function(warning) {
+              return console.log(warning);
+            });
+            stream.on('disconnect', function(disconnectMessage) {
+              return console.log(disconnectMessage);
+            });
+        });
       });
     }
   });
 
-    // Emits signal to the client telling them that the
-    // they are connected and can start receiving Tweets
-    socket.emit("connected");
-});
+  // When a "message" is received (click on the button), it's logged in the console
+  socket.on('message', function (message) {
+    console.log('Got message: ' + message);
+  });
 
+  socket.on("filter tweets", function (filterKeyword) {
+    console.log('Got keyword for filtering: ' + filterKeyword);
+    keyword = filterKeyword;
+  });
+
+  // Signal the client that they are connected and can start receiving tweets
+  console.log("Calling socket.emit connected");
+  socket.emit("connected");
+});
